1eabceb49f534ba3bfef0cbe3833506a662a64d0
Roberto Valcárcel <rovaldi.web@gmail.com>
2026-01-19 19:21:32 +0100

    chore: configure jest with typescript and react
    - Set up ts-jest with jsdom environment
    - Configure module path aliases
    - Add style mock for SCSS modules
    - Set up testing-library/jest-dom

---
+++ jest.setup.ts
@@ -0,0 +1,25 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom";
+import { TextEncoder, TextDecoder } from "util";
+
+Object.assign(global, { TextEncoder, TextDecoder });
+
+global.matchMedia =
+  global.matchMedia ||
+  function (query: string) {
+    return {
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: function () {},
+      removeListener: function () {},
+      addEventListener: function () {},
+      removeEventListener: function () {},
+      dispatchEvent: function () {
+        return false;
+      },
+    };
+  };
